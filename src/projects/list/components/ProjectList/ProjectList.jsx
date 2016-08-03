4bf7364d56ba3e4644f8f7d9f50f63cc38162540
vikasrohit <vikas.agarwal@appirio.com>
2016-08-03 16:42:09 +0530

    AS#163129582694739, Rich Data Table Component -- Used the RichDataTableHeader component to render the list header

--- src/projects/list/components/ProjectList/ProjectList.jsx
+++ src/projects/list/components/ProjectList/ProjectList.jsx
@@ -1,17 +1,56 @@
 import React, { PropTypes } from 'react'
 import ProjectListItem from '../ProjectListItem/ProjectListItem'
+import { RichDataTableHeader } from 'appirio-tech-react-components'
 
 require('./ProjectList.scss')
 
-const ProjectList = (({ projects }) => {
+const columns = [
+  { displayName : 'Type', key: 'type' },
+  { displayName : 'Projects', key: 'name' },
+  { displayName : 'Status', key: 'status', sorted: 'asc' },
+  { displayName : 'Status Date', key: 'statusDate' },
+  { displayName : 'Customer', key: 'customer' },
+  { displayName : 'Copilot', key: 'copilot' }
+]
+const sortColumns = {
+  name: [
+    {
+      key: 'timestamp',
+      value: 'Latest first',
+      order: 'desc'
+    },
+    {
+      key: 'timestamp',
+      value: 'Oldest first',
+      order: 'asc'
+    },
+    {
+      key: 'name',
+      value: 'Name A-Z',
+      order: 'asc'
+    },
+    {
+      key: 'name',
+      value: 'Name Z-A',
+      order: 'desc'
+    }
+  ]
+}
+const ProjectList = (({ projects, onSort }) => {
 
   const renderProject = (project, idx) => {
     return (<ProjectListItem project={ project } key={ idx } />)
   }
+  const sort = (column, option) => {
+    console.log('sorting on ' + option.key)
+    // TODO change state of column in the component state
+    column.sorted = option.order
+    onSort(option)
+  }
 
   return (
     <div className="project-list">
-      <ProjectListItem  headerOnly />
+      <RichDataTableHeader columns={ columns } sortColumns={ sortColumns } onSort={ sort }/>
       { projects.map(renderProject) }
     </div>
   )
